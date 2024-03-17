6215b0d9b816d24e46f44ec68ab0793ac33db54c
SyntaxModding <147758508+SyntaxModding@users.noreply.github.com>
2024-03-17 12:46:44 -0230

    Update 1.1.0
    Changed buttons for QR generators to something more modern and visually appealing. Added in 2 more QR code types: email and phone.

--- js/script.js
+++ js/script.js
@@ -45,12 +45,20 @@ const wifiQrBtn = document.getElementById("wifiQrBtn");
 const wifiQrDiv = document.getElementById("wifiQrDiv");
 const newQrBtns = document.querySelectorAll(".new-qr");
 const qrSrv = document.getElementById("qrSrv");
+const qrSrv2 = document.getElementById("qrSrv2");
+const phoneQrBtn = document.getElementById("phoneQrBtn");
+const phoneQrDiv = document.getElementById("phoneQrDiv");
+const emailQrBtn = document.getElementById("emailQrBtn");
+const emailQrDiv = document.getElementById("emailQrDiv");
 
 urlQrBtn.addEventListener("click", () => {
   urlQrDiv.style.display = "block";
   waQrDiv.style.display = "none";
   wifiQrDiv.style.display = "none";
   qrSrv.style.display = "none";
+  phoneQrDiv.style.display = "none";
+  qrSrv2.style.display = "none";
+  emailQrDiv.style.display = "none";
 });
 
 waQrBtn.addEventListener("click", () => {
@@ -58,6 +66,9 @@ waQrBtn.addEventListener("click", () => {
   urlQrDiv.style.display = "none";
   wifiQrDiv.style.display = "none";
   qrSrv.style.display = "none";
+  phoneQrDiv.style.display = "none";
+  qrSrv2.style.display = "none";
+  emailQrDiv.style.display = "none";
 });
 
 wifiQrBtn.addEventListener("click", () => {
@@ -65,6 +76,29 @@ wifiQrBtn.addEventListener("click", () => {
   waQrDiv.style.display = "none";
   urlQrDiv.style.display = "none";
   qrSrv.style.display = "none";
+  phoneQrDiv.style.display = "none";
+  qrSrv2.style.display = "none";
+  emailQrDiv.style.display = "none";
+});
+
+phoneQrBtn.addEventListener("click", () => {
+  phoneQrDiv.style.display = "block";
+  wifiQrDiv.style.display = "none";
+  waQrDiv.style.display = "none";
+  urlQrDiv.style.display = "none";
+  qrSrv.style.display = "none";
+  qrSrv2.style.display = "none";
+  emailQrDiv.style.display = "none";
+});
+
+emailQrBtn.addEventListener("click", () => {
+  emailQrDiv.style.display = "block";
+  phoneQrDiv.style.display = "none";
+  wifiQrDiv.style.display = "none";
+  waQrDiv.style.display = "none";
+  urlQrDiv.style.display = "none";
+  qrSrv.style.display = "none";
+  qrSrv2.style.display = "none";
 });
 
 function generateWhatsAppQR() {
@@ -148,6 +182,110 @@ function downloadWiFiQR() {
   const downloadLink = document.createElement("a");
   downloadLink.href = wifiQrCodeElement.src;
   downloadLink.download = "wifi_qr_code.png";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+function downloadWhatsAppQR() {
+  const waQrCodeElement = document.querySelector("#waQrCode img");
+  const downloadLink = document.createElement("a");
+  downloadLink.href = waQrCodeElement.src;
+  downloadLink.download = "whatsapp_qr_code.png";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+function generatePhoneQR() {
+  const qrphoneNumber = document.getElementById("qrphoneNumber").value;
+  const countryCode = document.getElementById("countryCode").value;
+  const phone = `${countryCode}${qrphoneNumber}`;
+  const phoneqrSize = document.getElementById("phoneqrSize").value;
+  const phoneqrColor = document.getElementById("phoneqrColor").value;
+  const phoneqrBackgroundColor = document.getElementById(
+    "wifiqrBackgroundColor"
+  ).value;
+  const phoneerror = document.getElementById("phoneError");
+
+  if (phone.trim() === "") {
+    phoneerror.style.display = "block";
+    phoneerror.innerHTML = "Please enter a valid phone number.";
+    setTimeout(() => {
+      phoneerror.style.display = "none";
+    }, 5000);
+    return;
+  }
+
+  const phoneUrl = `tel:${phone}`;
+
+  const phoneqrCodeContainer = document.getElementById("phoneQrCode");
+  phoneqrCodeContainer.innerHTML = "";
+
+  const phoneqrCode = new QRCode(phoneqrCodeContainer, {
+    text: phoneUrl,
+    width: phoneqrSize,
+    height: phoneqrSize,
+    colorDark: phoneqrColor,
+    colorLight: phoneqrBackgroundColor,
+    correctLevel: QRCode.CorrectLevel.H,
+  });
+
+  document.getElementById("phoneDownloadBtn").style.display = "block";
+}
+
+function downloadPhoneQR() {
+  const phoneQrCodeElement = document.querySelector("#phoneQrCode img");
+  const downloadLink = document.createElement("a");
+  downloadLink.href = phoneQrCodeElement.src;
+  downloadLink.download = "phone_qr_code.png";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
+
+function generateEmailQR() {
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("emailSubject").value;
+  const body = document.getElementById("emailBody").value;
+  const emailqrSize = document.getElementById("emailqrSize").value;
+  const emailqrColor = document.getElementById("emailqrColor").value;
+  const emailqrBackgroundColor = document.getElementById(
+    "emailqrBackgroundColor"
+  ).value;
+  const emailerror = document.getElementById("emailError");
+
+  if (email.trim() === "") {
+    emailerror.style.display = "block";
+    emailerror.innerHTML = "Please enter a valid email.";
+    setTimeout(() => {
+      emailerror.style.display = "none";
+    }, 5000);
+    return;
+  }
+
+  const emailUrl = `mailto:${email}?subject=${subject}&body=${body}`;
+
+  const emailqrCodeContainer = document.getElementById("emailQrCode");
+  emailqrCodeContainer.innerHTML = "";
+
+  const emailqrCode = new QRCode(emailqrCodeContainer, {
+    text: emailUrl,
+    width: emailqrSize,
+    height: emailqrSize,
+    colorDark: emailqrColor,
+    colorLight: emailqrBackgroundColor,
+    correctLevel: QRCode.CorrectLevel.H,
+  });
+
+  document.getElementById("emailDownloadBtn").style.display = "block";
+}
+
+function downloadEmailQR() {
+  const emailQrCodeElement = document.querySelector("#emailQrCode img");
+  const downloadLink = document.createElement("a");
+  downloadLink.href = emailQrCodeElement.src;
+  downloadLink.download = "email_qr_code.png";
   document.body.appendChild(downloadLink);
   downloadLink.click();
   document.body.removeChild(downloadLink);
